614171c19761284d62607dbbe66d0f1f73cf8c6d
Guilherme Varandas <jgvarandas@gmail.com>
2018-07-13 11:59:51 -0300

    Grading buttons style

--- src/components/Criterio.js
+++ src/components/Criterio.js
@@ -5,27 +5,33 @@ const Criterio = ({ criterio: { nome = '', nota = '' }, valor }) => {
   
   const handleClick = (event, action) => {
     event.preventDefault();
-    const novaNota = parseInt(event.target.innerText, 10);
-    if (novaNota !== nota) {
-      action({ valor, criterio: nome, nota: novaNota });
+    let novaNota = parseInt(event.target.innerText, 10);
+    if (novaNota === nota) {
+      novaNota = 0;
     }
+    action({ valor, criterio: nome, nota: novaNota });
   };
 
   return (
     <FeedbackConsumer>
       {({ actions: { atualizarCriterio } }) => (
         <li className="valor__criterio">
-          <span>{nome}</span>
-          <div className="valor__criterio__botoes" onClick={event => handleClick(event, atualizarCriterio)}>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
+          <div className="valor__criterio__nome">
+            <span>{nome}</span>
           </div>
-          <div>
+          <div
+            className="valor__criterio__botoes"
+            onClick={event => handleClick(event, atualizarCriterio)}
+          >
+            <button style={nota >= 1 && { backgroundColor: 'green' } || {}}>1</button>
+            <button style={nota >= 2 && { backgroundColor: 'green' } || {}}>2</button>
+            <button style={nota >= 3 && { backgroundColor: 'green' } || {}}>3</button>
+            <button style={nota >= 4 && { backgroundColor: 'green' } || {}}>4</button>
+            <button style={nota >= 5 && { backgroundColor: 'green' } || {}}>5</button>
+          </div>
+          {/* <div>
             <span>Nota Atual: {nota}</span>
-          </div>
+          </div> */}
         </li>
       )}
     </FeedbackConsumer>
